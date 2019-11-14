580a8823fbb8f7f6d5fa9ef6056806941e538da2
laportenier <lilly.portenier@gmail.com>
2019-11-14 15:37:05 -0500

    added bandsintown api functioning but getting lots of null results as well, alos added moment()

--- liri.js
+++ liri.js
@@ -6,6 +6,9 @@ var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
+var moment = require('moment');
+moment().format();
+
 var dashes = "\n--------------------------------------------------------"
 var command = process.argv[2];
 
@@ -24,36 +27,30 @@ if (command === "spotify-this-song") {
   songQuery();
 } else if (command === "movie-this") {
   movieQuery();
+} else if (command === "concert-this") {
+  concertQuery();
 }
 
 
 
-function songQuery(){
-  
+function songQuery() {
+
   var search = process.argv[3];
   if (search === undefined) {
     var search = "The Sign Ace of Base";
   }
-  spotify.search({ type: 'track', query: search }, function(err, data) {
+  spotify.search({ type: 'track', query: search }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     } else {
-     
-      // for(var i = 0; i < data.tracks.items[0].artists.length; i++) {
-      //   if(i === 0) {
-      //     console.log("Artist(s):    " + data.tracks.items[0].artists[i].name);
-      //   } else {
-      //     console.log("              " + data.tracks.items[0].artists[i].name);
-      //   }
-      // }
       console.log("Song:         " + data.tracks.items[0].name);
       console.log("Listen Here : " + data.tracks.items[0].preview_url);
       console.log("Album:        " + data.tracks.items[0].album.name);
-  }
+    }
     var text = "\nSong: " + data.tracks.items[0].name + "\nListen Here: " + data.tracks.items[0].preview_url + "\nAlbum: " + data.tracks.items[0].album.name + dashes;
 
-    fs.appendFile("random.txt", text, function (err){
-      if (err) throw err; 
+    fs.appendFile("random.txt", text, function (err) {
+      if (err) throw err;
       console.log(err);
     })
   });
@@ -65,10 +62,10 @@ function movieQuery() {
   if (movieName === undefined) {
     var movieName = "Mr.Nobody";
   }
-  var OMDBurl =  "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=df704d12";
-  // console.log(OMDBurl);
+  var OMDBurl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=df704d12";
+
   axios.get(OMDBurl).then(
-    function(response) {
+    function (response) {
       console.log("Title:         " + response.data.Title)
       console.log("Year:          " + response.data.Year)
       console.log("Rating:        " + response.data.Rated)
@@ -79,27 +76,46 @@ function movieQuery() {
       console.log("Plot:          " + response.data.Plot);
 
       var text = "\nTitle: " + response.data.Title + "\nYear: " + response.data.Year + "\nRating: " + response.data.Rated + "\nRotten Tomato: " + response.data.Ratings[1].Value + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nActors: " + response.data.Actors + "\nPlot: " + response.data.Plot + dashes;
-      fs.appendFile("random.txt", text, function (err){
-        if (err) throw err; 
+      fs.appendFile("random.txt", text, function (err) {
+        if (err) throw err;
         console.log(err);
-    
+
       })
     }
   )
-  .catch(function(error) {
-    if (error.response) {
+    .catch(function (error) {
+      if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    })
+};
+function concertQuery() {
+  var artist = "travis+scott"
 
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log("Error", error.message);
+  // var artist = process.argv[3];
+  axios.get("https://rest.bandsintown.com/artists/" + "celine+dion" + "/events?app_id=codingbootcamp").then(
+    function(response){
+    for (var i = 0; i < response.data.length; i++) {
+      var concertInfo = 
+        "\nVenue: " + response.data[i].venue.name +
+        "\nLocation: " + response.data[i].venue.city +
+        "\nDate: " + moment(response.data[i].datetime).format('LL') +
+        "\n" + dashes;
+      console.log(concertInfo);
+      fs.appendFile("random.txt", concertInfo, function (err) {
+        if (err) throw err;
+        console.log(err);
+      })
     }
-    console.log(error.config);
   })
 }
